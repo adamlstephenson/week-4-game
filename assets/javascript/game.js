4021f4ee1bf34cf5c2f3a6f23bdcc7f5b26762cd
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-08 23:08:27 -0600

    added psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Variables
+//========================================================
+
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+
+// Functions
+//========================================================
+
+$("document").ready(function() {
+
+    // Generate and show random number
+
+    // Show wins and losses
+
+    // Show totalScore
+
+    // Assign random numbers to  crystals
+
+    // Set up click events
+        // Print the value of each crystal that is clicked
+            // Add values to existing totalScore
+
+    // If totalScore === random number then add win and start over
+        // Else if score goes above random number then add loss anf start over
+
+
+
+
+});
+
+
+
+
+
+// Game Process
+//========================================================
