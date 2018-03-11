f609924780e592b7a2bce8cd814889c4fe680736
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-11 16:10:15 -0500

    added wins and loss statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,18 @@
-// Variables
-//========================================================
-
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
-
 
 // Functions
 //========================================================
 
 $("document").ready(function() {
 
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+
     // function startGame() {
                 // Generate and show random number
 
-        $("#random-number").text(Math.floor(Math.random() * 101) +19);
+    var randomNumber = Math.floor(Math.random() * 101) +19;
+    $("#random-number").text(randomNumber);
 
                 // Assign random numbers to  crystals
 
@@ -28,7 +26,7 @@ $("document").ready(function() {
         $("#sapphire").val(sapphire);
         console.log(sapphire);
         var zircon = Math.floor(Math.random() * 11) +1;
-        $("#zircon").val(zircon);
+        $("#zircon").val(zircon)
         console.log(zircon);
 
     // }
@@ -45,13 +43,31 @@ $("document").ready(function() {
     $("#scorecounter").text(totalScore);
 
     // Set up click events
-
-        $("#crystals").on("click", "img", function (event) {
-           
-                $("#scorecounter").text(zircon);
-        });;
         // Print the value of each crystal that is clicked
             // Add values to existing totalScore
+
+        $("#crystals").on("click", "img", function(event) {
+           
+                $("#scorecounter").html(totalScore + event.value);
+
+                   if (totalScore === randomNumber) {
+                       $("#result").text("You won!");
+                       wins++;
+                       $("#wins").text(wins);
+                      // gameStart();
+                   } 
+                   else if (totalScore > randomNumber) {
+                       $("#result").text("You lost");
+                       losses--;
+                       $("#losses").text(losses);
+                       // gameStart ();
+                   }
+
+
+
+
+        });
+
 
     // If totalScore === random number then add win and start over
         // Else if score goes above random number then add loss anf start over
